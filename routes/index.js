7801277629e66329a4b230420d90201de2f243f4
Carlos Gámez <hardpx@gmail.com>
2022-11-03 13:31:13 -0600

    created error templates, and aded stories route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+const Story = require("../models/Story");
+const { rawListeners } = require("../models/User");
+
 // @description Login/Landing page
 // @route GET /
 router.get("/", ensureGuest, (req, res) => {
@@ -9,10 +13,17 @@ router.get("/", ensureGuest, (req, res) => {
 
 // @description Dashboard
 // @route GET /dashboard
-router.get("/dashboard", ensureAuth, (req, res) => {
-  res.render("dashboard", {
-    name: req.user.firstName,
-  });
+router.get("/dashboard", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render("dashboard", {
+      name: req.user.firstName,
+      stories,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
 });
 
 module.exports = router;
