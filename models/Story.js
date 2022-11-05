abee92ef7d4b4dd54eb1b2866cbac509be14498a
Carlos Gámez <hardpx@gmail.com>
2022-11-04 21:28:41 -0600

    added a public stories page and fixed some bugs

--- models/Story.js
+++ models/Story.js
@@ -15,9 +15,10 @@ const StorySchema = new mongoose.Schema({
     defaul: "public",
     enum: ["public", "private"],
   },
-  use: {
+  user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
+    required: true,
   },
   createdAt: {
     type: Date,
