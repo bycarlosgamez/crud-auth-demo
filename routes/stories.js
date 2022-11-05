6ec249ef94226c2b0592333427bab997b4fb26eb
Carlos Gámez <hardpx@gmail.com>
2022-11-04 20:49:15 -0600

    create hbs helper file to format date

--- routes/stories.js
+++ routes/stories.js
@@ -3,11 +3,25 @@ const router = express.Router();
 const { ensureAuth } = require("../middleware/auth");
 
 const Story = require("../models/Story");
+const { rawListeners } = require("../models/User");
 
 // @description     Show add page
 // @route           GET /stories/add
 router.get("/add", ensureAuth, (req, res) => {
   res.render("stories/add");
+});
+
+// @description     Process add form
+// @route           POST /stories
+router.post("/", ensureAuth, async (req, res) => {
+  try {
+    req.body.user = req.user.id;
+    await Story.create(req.body);
+    res.redirect("/dashboard");
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
 });
 
 module.exports = router;
