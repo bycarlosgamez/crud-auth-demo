f63bca5db55f5b2c1b1c10f7d84b96048d87887f
Carlos Gámez <hardpx@gmail.com>
2022-11-06 19:11:28 -0600

    added a single story route and user id stories

--- routes/stories.js
+++ routes/stories.js
@@ -41,6 +41,25 @@ router.get("/", ensureAuth, async (req, res) => {
   }
 });
 
+// @description     Show singe story
+// @route           GET /stories/:id
+router.get("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate("user").lean();
+
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    res.render("stories/show", {
+      story,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("error/404");
+  }
+});
+
 // @description     Show edit page
 // @route           GET /stories/edit/:id
 router.get("/edit/:id", ensureAuth, async (req, res) => {
@@ -97,6 +116,26 @@ router.delete("/:id", ensureAuth, async (req, res) => {
   try {
     await Story.remove({ _id: req.params.id });
     res.redirect("/dashboard");
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
+});
+
+// @desc    User stories
+// @route   GET /stories/user/:userId
+router.get("/user/:userId", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: "public",
+    })
+      .populate("user")
+      .lean();
+
+    res.render("stories/index", {
+      stories,
+    });
   } catch (err) {
     console.error(err);
     res.render("error/500");
